8c5001cf3d827be78d36ae6cd2d13585b761bffe
Elisabetta Petruzzi Merli <elisabetta.petruzzimerli@gmail.com>
2026-07-07 09:48:59 +0200

    Implement user registration service

--- src/main/java/com/elisabetta/taskmanager/service/UserServiceImpl.java
+++ src/main/java/com/elisabetta/taskmanager/service/UserServiceImpl.java
@@ -1,15 +1,44 @@
 package com.elisabetta.taskmanager.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.elisabetta.taskmanager.dto.RegisterUserDto;
 import com.elisabetta.taskmanager.model.User;
+import com.elisabetta.taskmanager.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository,
+                           PasswordEncoder passwordEncoder) {
+
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public User registerUser(RegisterUserDto registerUserDto) {
 
-        return null;
+        if (userRepository.findByEmail(registerUserDto.getEmail()).isPresent()) {
 
+            throw new RuntimeException("Email già registrata");
+        }
+
+        User user = new User();
+
+        user.setUsername(registerUserDto.getUsername());
+
+        user.setEmail(registerUserDto.getEmail());
+
+        user.setPassword(
+                passwordEncoder.encode(registerUserDto.getPassword())
+        );
+
+        return userRepository.save(user);
     }
 }
