b3941bd53372d4eefc9d59052595293edb357577
Elisabetta Petruzzi Merli <elisabetta.petruzzimerli@gmail.com>
2026-07-07 10:42:52 +0200

    Implement user registration

--- src/main/java/com/elisabetta/taskmanager/controller/RegisterController.java
+++ src/main/java/com/elisabetta/taskmanager/controller/RegisterController.java
@@ -3,11 +3,20 @@ package com.elisabetta.taskmanager.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.elisabetta.taskmanager.dto.RegisterUserDto;
+import com.elisabetta.taskmanager.service.UserService;
 
 @Controller
 public class RegisterController {
+
+    private final UserService userService;
+
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/register")
     public String showRegisterForm(Model model) {
@@ -15,6 +24,15 @@ public class RegisterController {
         model.addAttribute("registerUserDto", new RegisterUserDto());
 
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(
+            @ModelAttribute RegisterUserDto registerUserDto) {
+
+        userService.registerUser(registerUserDto);
+
+        return "redirect:/";
     }
 
 }
