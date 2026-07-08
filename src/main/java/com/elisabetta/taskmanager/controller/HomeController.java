0fec8735879fce534f6e60bfaf97f18536a15319
Elisabetta Petruzzi Merli <elisabetta.petruzzimerli@gmail.com>
2026-07-08 09:45:42 +0200

    Add logout and authenticated homepage

--- src/main/java/com/elisabetta/taskmanager/controller/HomeController.java
+++ src/main/java/com/elisabetta/taskmanager/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.elisabetta.taskmanager.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Authentication authentication, Model model) {
+
+        if (authentication != null && authentication.isAuthenticated()
+                && !"anonymousUser".equals(authentication.getName())) {
+
+            model.addAttribute("username", authentication.getName());
+        }
+
         return "index";
     }
-
 }
