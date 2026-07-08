fc409536a281727406dc047baac869d32a706f81
Elisabetta Petruzzi Merli <elisabetta.petruzzimerli@gmail.com>
2026-07-08 11:09:28 +0200

    Create TaskRepository

--- src/main/java/com/elisabetta/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/elisabetta/taskmanager/repository/TaskRepository.java
@@ -1,9 +1,14 @@
 package com.elisabetta.taskmanager.repository;
 
+import com.elisabetta.taskmanager.model.Task;
+import com.elisabetta.taskmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.elisabetta.taskmanager.model.Task;
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser(User user);
+    List<Task> findByUserOrderByCreationDateDesc(User user);
 
 }
