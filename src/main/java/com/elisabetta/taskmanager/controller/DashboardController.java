486e1c02aa27bfd3c0a131ff01337abe80164979
Elisabetta Petruzzi Merli <elisabetta.petruzzimerli@gmail.com>
2026-07-08 11:34:39 +0200

    Display user tasks in dashboard

--- src/main/java/com/elisabetta/taskmanager/controller/DashboardController.java
+++ src/main/java/com/elisabetta/taskmanager/controller/DashboardController.java
@@ -1,16 +1,40 @@
 package com.elisabetta.taskmanager.controller;
 
+import com.elisabetta.taskmanager.model.User;
+import com.elisabetta.taskmanager.repository.UserRepository;
+import com.elisabetta.taskmanager.service.TaskService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
-    @GetMapping("/dashboard")
-    public String dashboard() {
+    private final TaskService taskService;
+    private final UserRepository userRepository;
 
-        return "dashboard";
+    public DashboardController(TaskService taskService,
+                               UserRepository userRepository) {
 
+        this.taskService = taskService;
+        this.userRepository = userRepository;
     }
 
+    @GetMapping("/dashboard")
+    public String dashboard(Authentication authentication,
+                            Model model) {
+
+        User user = userRepository
+                .findByEmail(authentication.getName())
+                .orElseThrow();
+
+        model.addAttribute("tasks",
+                taskService.getTasksByUser(user));
+
+        model.addAttribute("username",
+                user.getUsername());
+
+        return "dashboard";
+    }
 }
